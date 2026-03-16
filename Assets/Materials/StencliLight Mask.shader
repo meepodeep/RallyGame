ce713cd4ad3f9882c4976f7290e7b6cb6dbc7152
meepodeep <nbltmepodep@gmail.com>
2026-03-15 20:35:38 -0400

    made anoither level and home scren

---
+++ Assets/Materials/StencliLight Mask.shader
@@ -0,0 +1,30 @@
+Shader "Custom/StencilLight Mask"
+{
+    SubShader
+    {
+        Tags
+        {
+            "RenderType" = "Transparent"
+            "Queue" = "Transparent"
+            "RenderPipeline" = "UniversalPipeline"
+        }
+        
+        LOD 100
+        
+        Pass
+        {           
+            Ztest Greater
+            Zwrite off
+            Cull Off
+            Colormask 0
+            Stencil
+            {
+                Comp Greater
+                Ref 1
+                Pass Replace
+            }
+        }
+        
+        
+    }
+}
---
+++ Assets/Scripts/textScroll.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class textScroll : MonoBehaviour {
+
+ public TextMeshProUGUI text;
+ public float scrollSpeed = 10.0f;
+
+ private TextMeshProUGUI cloneText;
+ private RectTransform textRectTransform;
+ private string sourceText;
+ private string tempText;
+
+ // Use this for initialization
+ void Awake () {
+  textRectTransform = text.GetComponent<RectTransform>();
+        
+  cloneText = Instantiate(text) as TextMeshProUGUI;
+  RectTransform cloneRectTransform = cloneText.GetComponent<RectTransform>();
+  cloneRectTransform.SetParent(textRectTransform);
+  cloneRectTransform.anchorMin = new Vector2(1, 0.5f);
+  cloneRectTransform.localPosition = new Vector3(text.preferredWidth, 0, cloneRectTransform.position.z);
+  cloneRectTransform.localEulerAngles = new Vector3(0,0,0);
+  cloneRectTransform.localScale = new Vector3(1, 1, 1);
+  cloneText.text = text.text;  
+
+ }
+    void OnEnable()
+    {
+        StartCoroutine(Start());
+    }
+    private IEnumerator Start()
+ {
+  
+  float width = text.preferredWidth;      
+  Vector3 startPosition = textRectTransform.localPosition;
+  float scrollPosition = 0;
+
+        while (true)
+  {
+   textRectTransform.localPosition = new Vector3(-scrollPosition % width, startPosition.y, startPosition.z);
+   textRectTransform.localEulerAngles = new Vector3(0,0,0);
+   scrollPosition += scrollSpeed * 20 * Time.deltaTime;         
+   yield return null;
+  }      
+ }
+    void OnDisable()
+    {
+        StopCoroutine(Start());
+    }
+}
